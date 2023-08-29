f48dd63d8a3df15cf27ba2879f9e71426e45c1d9
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-29 10:04:08 +0530

    designed dynamic layout for showing saved cards

--- Client/luhn-validation/src/components/UserCardList.jsx
+++ Client/luhn-validation/src/components/UserCardList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Container,
   Typography,
@@ -6,24 +7,26 @@ import {
   ListItem,
   ListItemText,
   Divider,
+  Button,
 } from '@mui/material';
+import { setCards } from '../state/index.js';
 
-function UserCardList() {
+const UserCardList = ({ userId }) => {
+  const dispatch = useDispatch();
   const [userCards, setUserCards] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
-
+  const token = useSelector((state) => state.token);
 
   const fetchUserCards = async () => {
     try {
-      const response = await fetch('YOUR_BACKEND_API_URL/cards');
-      
-      if (!response.ok) {
-        throw new Error('Failed to fetch user cards.');
-      }
+      const response = await fetch(`http://localhost:3001/cards/${userId}/cards`, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
       const data = await response.json();
+      dispatch(setCards(data));
       setUserCards(data);
       setIsLoading(false);
     } catch (error) {
@@ -32,9 +35,32 @@ function UserCardList() {
     }
   };
 
+  const handleDeleteCard = async (cardId) => {
+    const shouldDelete = window.confirm('Are you sure you want to delete this card?');
+
+    if (!shouldDelete) {
+      return; 
+    }
+
+    try {
+      const response = await fetch(`http://localhost:3001/cards/${cardId}`, {
+        method: 'DELETE',
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (response.ok) {
+        setUserCards((prevUserCards) => prevUserCards.filter((card) => card._id !== cardId));
+      } else {
+        console.error('Failed to delete card.');
+      }
+    } catch (error) {
+      console.error('Error deleting card:', error);
+    }
+  };
+
   useEffect(() => {
     fetchUserCards();
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Container maxWidth="md">
@@ -43,9 +69,7 @@ function UserCardList() {
       </Typography>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
-      {!isLoading && !error && userCards.length === 0 && (
-        <p>No cards found.</p>
-      )}
+      {!isLoading && !error && userCards.length === 0 && <p>No cards found.</p>}
       {!isLoading && !error && userCards.length > 0 && (
         <List>
           {userCards.map((card) => (
@@ -55,6 +79,15 @@ function UserCardList() {
                   primary={`Card Number: ${card.cardNumber}`}
                   secondary={`Card Holder: ${card.cardHolder}`}
                 />
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={() => handleDeleteCard(card._id)}
+                  style={{ marginLeft: '150px' }}
+
+                >
+                  Delete
+                </Button>
               </ListItem>
               <Divider />
             </React.Fragment>
@@ -63,6 +96,6 @@ function UserCardList() {
       )}
     </Container>
   );
-}
+};
 
 export default UserCardList;

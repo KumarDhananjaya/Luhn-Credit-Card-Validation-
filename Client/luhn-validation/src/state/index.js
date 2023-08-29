cde387351bf7c83bdf3ae7ed0bfe556df670e809
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-29 10:02:42 +0530

    redux state added to store card details

--- Client/luhn-validation/src/state/index.js
+++ Client/luhn-validation/src/state/index.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState ={
     user: null,
     token: null,
+    cards: [],
 };
 
 export const authSlice = createSlice({
@@ -17,8 +18,18 @@ export const authSlice = createSlice({
             state.user = null;
             state.token = null;
         },
+        setCards: (state, action) => {
+            state.cards = action.payload.cards;
+        },
+        setCard: (state, action) => {
+            const updatedCards = state.cards.map((card) => {
+                if(card._id === action.payload.card._id) return action.payload.card;
+                return card;
+            });
+            state.cards = updatedCards;
+        }
     }
 })
 
-export const { setLogin, setLogout} = authSlice.actions;
+export const { setLogin, setLogout, setCards, setCard} = authSlice.actions;
 export default authSlice.reducer;
